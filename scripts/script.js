9711bacd0279637d27b72c54effb72f6aef7496b
Niklas Albinsson <albinssonniklas@gmail.com>
2017-07-14 23:24:18 +0200

    Mycket updates

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const inputBox = document.querySelector('.inputBox');
+const contactName = document.querySelector('#name');
+const contactEmail = document.querySelector('#email');
+const contactTele = document.querySelector('#telefon');
+const contactSubj = document.querySelector('.ämne');
+const contactSalj = document.querySelector('.säljare');
+
+// Bygg en funktion som tar värderna från dessa variablar och sätt ihop dem till
+// en emailfunktion som skickar ett email till den specifika mailen med värderna
+
+function mailDetails() {
+
+};
+
+function sendMail() {
+
+};
