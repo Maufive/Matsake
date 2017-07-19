af27a2895f59aa3ad7b48d00b66fdab4f1f7eb86
Niklas Albinsson <albinssonniklas@gmail.com>
2017-07-19 16:01:43 +0200

    Stor update på utbud

---
+++ scripts/utbud_hover.js
@@ -0,0 +1,22 @@
+const textbox = document.querySelectorAll('.textbox');
+
+function textboxEnter() {
+  this.classList.add('hover-enter');
+  setTimeout(() => this.classList.contains('hover-enter') && this.classList.add('hover-active'), 150);
+  this.children[1].classList.add('element-display');
+  setTimeout(() => this.children[1].classList.contains('element-display') && this.children[1].classList.add('element-display-active'), 150);
+
+  console.log();
+}
+
+function textboxLeave() {
+  if(this.classList.contains('hover-enter')) {
+    this.classList.remove('hover-enter', 'hover-active');
+  }
+  if(this.children[1].classList.contains('element-display')) {
+    this.children[1].classList.remove('element-display', 'element-display-active');
+  }
+}
+
+textbox.forEach(textbox => textbox.addEventListener('mouseenter', textboxEnter));
+textbox.forEach(textbox => textbox.addEventListener('mouseleave', textboxLeave));
