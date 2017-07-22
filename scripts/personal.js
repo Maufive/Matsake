681f435693163077daf2114cc181b1ebfdd5d4f7
Niklas Albinsson <albinssonniklas@gmail.com>
2017-07-22 14:33:58 +0200

    Lagt till om oss-delen

---
+++ scripts/personal.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true
+  });
+});
