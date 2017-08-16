8cac92a967cda81b4f37cd6000c6d4336c324cb4
Niklas Albinsson <albinssonniklas@gmail.com>
2017-08-16 05:33:53 +0200

    Lagt till Media + Optimerat för safari

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,11 @@
-var gulp = require('gulp');
-var htmltidy = require('gulp-htmltidy');
-var autoprefixer = require('gulp-autoprefixer');
-var csslint = require('gulp-csslint');
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', ['html', 'css'] );
-
-gulp.task('html', function() {
-  return gulp.src('./index.html')
-        .pipe(htmltidy())
-        .pipe(gulp.dest('build'));
-});
-
-gulp.task('css', function() {
-  return gulp.src('./styles/style.css')
-        .pipe(csslint())
-        .pipe(csslint.formatter('compact'))
+gulp.task('default', () =>
+    gulp.src('./styles/mobile.css')
         .pipe(autoprefixer({
-          browsers: ['last 2 versions'],
-          cascade: false
+            browsers: ['last 2 versions'],
+            cascade: false
         }))
-        .pipe(gulp.dest('build'));
-});
+        .pipe(gulp.dest('dist'))
+);
---
+++ gulpfile2.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var htmltidy = require('gulp-htmltidy');
+var autoprefixer = require('gulp-autoprefixer');
+var csslint = require('gulp-csslint');
+
+gulp.task('default', ['html', 'css'] );
+
+gulp.task('html', function() {
+  return gulp.src('./hitta_hit.html')
+        .pipe(htmltidy())
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('css', function() {
+  return gulp.src('./styles/style.css')
+        .pipe(csslint())
+        .pipe(csslint.formatter('compact'))
+        .pipe(autoprefixer({
+          browsers: ['last 2 versions'],
+          cascade: false
+        }))
+        .pipe(gulp.dest('build'));
+});
